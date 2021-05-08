dc62b41fbd56cbec160136576fb6efc86c746a59
Анастасия Крутицкая <anastasia@MacBook-Pro-Anastasia.local>
2021-05-08 09:30:51 +0300

    first commit

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,12 @@
+import sortByHealth, { characters } from '../app.js';
+
+const charactersSorted = [
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'мечник', health: 10 },
+];
+
+test('sortByHealth', () => {
+  sortByHealth(characters);
+  expect(characters).toEqual(charactersSorted);
+});
---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+export default function sortByHealth(arr) {
+  arr.sort((a, b) => (a.health > b.health ? -1 : 1));
+}
+
+export const characters = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
